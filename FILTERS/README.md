8df0ac14f8858097f1096d0b68def67d169b0867
Vaasu Bisht <92209437+vaasu2002@users.noreply.github.com>
2022-03-13 08:48:02 +0530

    Delete FILTERS directory

--- FILTERS/README.md
+++
@@ -1,4 +0,0 @@
-# BASICS
-1)    Read Images
-2)    Use Webcam
-3)    **FILTERS** - Gray Image, GaussianBlur, Canny , 
--- FILTERS/black image.py
+++
@@ -1,4 +0,0 @@
-blank = np.zeros((500, 500, 3), np.uint8)
-cv2.imshow('blank', blank)
-plt.imshow(blank)
-cv2.waitKey(0)
